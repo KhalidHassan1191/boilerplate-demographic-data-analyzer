78c7db059d693e269176381a2796df2d9584c62d
KhalidHassan1191 <khalidhassan1191@gmail.com>
2024-06-24 06:24:15 +0600

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,42 +3,42 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = pd.Series(df["race"]).value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df["age"].mean(), 2)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round((pd.Series(df["education"].value_counts(), index = ["Bachelors"]).iloc[0]/df["education-num"].sum())*100, 2)    
 
-    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # What percentage of people with advanced education (Bachelors, Masters, or Doctorate) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    # with and without Bachelors, Masters, or Doctorate
+    higher_education = len(df[(df["education"].isin(['Bachelors', 'Masters', 'Doctorate'])) & (df["salary"] == '>50K')])
+    lower_education = pd.Series(df["salary"].value_counts(), index = [">50K"]).iloc[0] - higher_education
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    higher_education_rich = round((higher_education/pd.Series(df["salary"].value_counts(), index = [">50K"]).iloc[0]) * 100, 2)
+    lower_education_rich = round((lower_education/pd.Series(df["salary"].value_counts(), index = [">50K"]).iloc[0]) * 100, 2)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = len(df[(df["hours-per-week"] == df["hours-per-week"].min()) & (df["salary"] == '>50K')])
 
-    rich_percentage = None
-
+    rich_percentage = round((num_min_workers/pd.Series(df["salary"].value_counts(), index = [">50K"]).iloc[0]) * 100, 2)
+    
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    highest_earning_country = ((df[df['salary'] == '>50K']['native-country'].value_counts() / df['native-country'].value_counts()) * 100).idxmax()
+    highest_earning_country_percentage =round(((df[df['salary'] == '>50K']['native-country'].value_counts() / df['native-country'].value_counts()) * 100).max(), 2)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    top_IN_occupation = (df[(df['salary'] == '>50K') & (df['native-country'] == 'India')]['occupation'].value_counts()).idxmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
@@ -47,7 +47,7 @@ def calculate_demographic_data(print_data=True):
         print("Average age of men:", average_age_men)
         print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
         print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
-        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        print(f"Percentage without lower education that earn >50K: {lower_education_rich}%")
         print(f"Min work time: {min_work_hours} hours/week")
         print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
         print("Country with highest percentage of rich:", highest_earning_country)
